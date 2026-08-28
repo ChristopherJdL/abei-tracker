a0a45da4f1266d246ea955df22e58572ff1c919b
christopherjdl <christopher.jdl@outlook.com>
2026-08-28 17:43:04 +0100

    fix: switch carto tiles to standard OSM with css invert filter for free dark map

--- src/components/AbeiMap.tsx
+++ src/components/AbeiMap.tsx
@@ -27,18 +27,17 @@ const MAP_STYLE: StyleSpecification = {
   version: 8,
   name: 'abei-arctic-dark',
   sources: {
-    'carto-dark': {
+    'osm-standard': {
       type: 'raster',
       tiles: [
-        'https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
-        'https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
-        'https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
-        'https://d.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}@2x.png',
+        'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png',
       ],
       tileSize: 256,
       attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/">CARTO</a>',
-      maxzoom: 18,
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      maxzoom: 19,
     },
   },
   layers: [
@@ -48,9 +47,9 @@ const MAP_STYLE: StyleSpecification = {
       paint: { 'background-color': '#071f2e' },
     },
     {
-      id: 'carto-dark',
+      id: 'osm-standard',
       type: 'raster',
-      source: 'carto-dark',
+      source: 'osm-standard',
       paint: {
         'raster-fade-duration': 0,
         'raster-opacity': 1,
